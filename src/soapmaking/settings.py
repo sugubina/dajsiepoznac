13f77476b51873dcfd9698ee5d6b19e6d3db050a
kjablonska <kjablonska@ownedoutcomes.com>
2017-03-16 15:11:57 +0100

    front

--- src/soapmaking/settings.py
+++ src/soapmaking/settings.py
@@ -101,5 +101,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 MEDIA_URL = '/media/'
